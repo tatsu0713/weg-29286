bcf832253ed48b3a782ed80670048b58a31f0c4b
tatsu0713 <kwbttk@icloud.com>
2020-10-02 16:05:06 +0900

    投稿の単体テストコード

---
+++ app/javascript/index.js
@@ -0,0 +1,29 @@
+// function pullDown() {
+
+//   const pullDownButton = document.getElementById("lists")
+//   const pullDownParents = document.getElementById("pull-down")
+//   const pullDownChild = document.querySelectorAll(".pull-down-list")
+
+  
+  
+//   pullDownButton.addEventListener('click', function(){
+//     if (pullDownParents.getAttribute("style") == "display:block;"){
+//       pullDownParents.removeAttribute("style", "display:block;")
+//     } else {
+//       pullDownParents.setAttribute("style", "display:block;")
+//     }
+//   })
+
+//   pullDownChild.forEach(function(list){
+//     list.addEventListener('mouseover', function(){
+//       list.setAttribute("style", "background-color:black;")
+//     })
+//     list.addEventListener('mouseout', function(){
+//       list.removeAttribute("style", "background-color:black;")
+//     })
+//     list.addEventListener('click', function(){
+//     })
+//   })
+// }
+
+// window.addEventListener('load', pullDown);
--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :tweet do
-    
+    brand_id            {2}
+    type_id             {2}
+    model_year_id       {2}
+    title               {'海の見える街'}
+    caption             {'aaaaaaaaaaaaaa'}
+    user
   end
 end
