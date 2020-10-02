eb2c3d7481183706650d859adc11653f59dd176c
tatsu0713 <kwbttk@icloud.com>
2020-10-02 12:50:17 +0900

    ビューの整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweets
+  belongs_to_active_hash :favorite_car
+
+  validates :nickname, presence: true
+  validates :password, length: { minimum: 8 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  validates :favorite_car_id, numericality: { other_than: 1, message: 'を選択してください'}
 end
