a2b9cea98134189d9de3881a8518b96d2901abcb
tatsu0713 <kwbttk@icloud.com>
2020-10-02 15:14:53 +0900

    投稿内容の保存

--- db/migrate/20201002051854_create_tweets.rb
+++ db/migrate/20201002051854_create_tweets.rb
@@ -2,8 +2,11 @@ class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
       t.references :user,          foreign_key: true
-      t.integer :category_id,      null: false
-      t.integer :tweet_status_id,  null: false
+      t.integer :brand_id,         null: false
+      t.integer :type_id,          null: false
+      t.integer :model_year_id,    null: false
+      t.string :title,             null: false
+      t.text :caption,             null: false
       t.timestamps
     end
   end
