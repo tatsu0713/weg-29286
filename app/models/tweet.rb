12d823904c93cde88789703d362dacd74083bbbf
tatsu0713 <kwbttk@icloud.com>
2020-10-02 14:40:27 +0900

    投稿の確認

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,15 +2,14 @@ class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_many :comments
-  belongs_to_active_hash :brand
-  belongs_to_active_hash :type
-  belongs_to_active_hash :model_year
+  belongs_to_active_hash :category
+  belongs_to_active_hash :tweet_status
   has_one_attached :image
 
 
-  validates :title, presence: true
-  validates :caption, presence: true
+  # validates :title, presence: true
+  # validates :caption, presence: true
   validates :image, presence: { message: 'を選択してください'}
 
-  validates :brand_id, :type_id, :model_year_id, numericality: { other_than: 1, message: 'を選択してください'}
+  validates :category_id, :tweet_status_id, numericality: { other_than: 1, message: 'を選択してください'}
 end
