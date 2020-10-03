ff5d802c393ea5ff3e5e1a43664054c4d03bf1df
tatsu0713 <kwbttk@icloud.com>
2020-10-03 12:02:40 +0900

    投稿のビュー整理

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
+    @tweet = Tweet.all.order('created_at DESC')
   end
 
   def new
@@ -31,4 +32,7 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:brand_id, :type_id, :model_year_id, :title, :caption, :image).merge(user_id: current_user.id)
   end
 
+  # def set_tweet
+  #   @tweet = Tweet.find(params[:id])
+  # end
 end
