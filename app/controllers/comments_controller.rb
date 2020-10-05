e44ce52e7304c145616a96a326c459e48d347fb2
tatsu0713 <kwbttk@icloud.com>
2020-10-05 21:04:51 +0900

    comment機能のビュー

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    # Comment.create(comment_params)
+    comment = Comment.create(comment_params)
+    redirect_to tweet_path(comment.tweet.id)
   end
 
   private
 
-  # def comment_params
-  #   params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
-  # end
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
