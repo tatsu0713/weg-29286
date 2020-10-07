2344a37351eadd6231c58bc394283aa0496d4bbf
tatsu0713 <kwbttk@icloud.com>
2020-10-07 12:44:45 +0900

    設定機能作成

---
+++ app/javascript/show.js
@@ -0,0 +1,27 @@
+function pullDown() {
+
+  const pullDownButton = document.getElementById("set-lists")
+  const pullDownParents = document.getElementById("set-down")
+  const pullDownChild = document.querySelectorAll(".set-down-list")
+
+  pullDownButton.addEventListener('click', function(){
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+
+  pullDownChild.forEach(function(list){
+    list.addEventListener('mouseover', function(){
+      list.setAttribute("style", "background-color:black;")
+    })
+    list.addEventListener('mouseout', function(){
+      list.removeAttribute("style", "background-color:black;")
+    })
+      // list.addEventListener('click', function(){
+    // })
+  })
+}
+
+window.addEventListener('load', pullDown)
