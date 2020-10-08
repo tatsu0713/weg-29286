58907da4f5f8a1a7ae5b6fb4c7a09b4848b69655
tatsu0713 <kwbttk@icloud.com>
2020-10-09 03:57:09 +0900

    jquery修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,46 @@
 class LikesController < ApplicationController
-  before_action :set_tweet, only: [:create, :destroy]
+  # before_action :set_tweet, only: [:create, :destroy]
 
+  # def create
+  #   @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
+  # end
+
+  # def destroy
+  #   like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+  #   like.destroy
+  # end
+
+  # private
+
+  # def set_tweet
+  #   @tweet = Tweet.find(params[:tweet_id])
+  # end
+
+  before_action :set_tweet, only: [:create, :destroy]
   def create
-    @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
+    @like = current_user.likes.create(like_params)
+
+    respond_to do |format|
+      format.html { redirect_to @tweet }
+      format.js
+    end
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
-    like.destroy
+    @like = Like.find_by(like_params, user_id: current_user.id)
+    @like.destroy
+    respond_to do |format|
+      format.html { redirect_to @tweet }
+      format.js
+    end
   end
 
   private
-
   def set_tweet
     @tweet = Tweet.find(params[:tweet_id])
   end
+  def like_params
+    params.permit(:tweet_id)
+  end
+  
 end
