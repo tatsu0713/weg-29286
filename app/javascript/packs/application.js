8c3f5a27b54e38119157806ffea72f3cc6a38b89
tatsu0713 <kwbttk@icloud.com>
2020-10-09 04:39:11 +0900

    usersコントローラー作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../show")
-// require("jquery").start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :comments
     resources :likes, only: [:create, :destroy]
   end
+  resources :users, only: :show
 end
