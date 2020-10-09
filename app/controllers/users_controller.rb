f4f31a2b1049ac1bc2ed7c6991d76521c2ff3021
tatsu0713 <kwbttk@icloud.com>
2020-10-09 12:47:55 +0900

    マイページのビュー整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
     @tweets = current_user.tweets
+    @user = User.find(params[:id])
+    @user = current_user.favorite_car[:name]
   end
 end
